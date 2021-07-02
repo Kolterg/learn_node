131730bb28cd96c712618e9e57cf6c30b10c36c8
Kolterg <mark.reta99@gmail.com>
2021-07-02 21:04:07 +0300

    Task5 in progress.

--- Task5/constants/index.js
+++ Task5/constants/index.js
@@ -1,6 +1,8 @@
-module.exports.constants = require('./constant');
-module.exports.userRolesEnum = require('./user-roles.enum');
-module.exports.databaseTablesEnum = require('./database-tables.enum');
-module.exports.responseCodesEnum = require('./response-codes.enum');
-module.exports.errorMessages = require('./error-messages');
-module.exports.regexp = require('./regexp');
+module.exports = {
+    constants: require('./constant'),
+    userRolesEnum: require('./user-roles.enum'),
+    databaseTablesEnum: require('./database-tables.enum'),
+    responseCodesEnum: require('./response-codes.enum'),
+    errorMessages: require('./error-messages'),
+    regexp: require('./regexp'),
+};
