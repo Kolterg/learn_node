9cfd678ce4d70f5d3f7e95134b2e993e7e29eac6
Kolterg <mark.reta99@gmail.com>
2021-07-04 17:46:12 +0300

    Task5 is done.

--- Task5/routes/user.router.js
+++ Task5/routes/user.router.js
@@ -11,7 +11,7 @@ router.delete('/:userId', userMiddleware.checkIsUserPresent, userController.dele
 
 router.get('/:userId', userMiddleware.checkIsUserPresent, userController.getUserById);
 
-router.post('/:userId', userMiddleware.checkIsUserPresent, userMiddleware.checkIsUserValid,
+router.patch('/:userId', userMiddleware.checkIsUserPresent, userMiddleware.checkIsUserValid,
     userMiddleware.checkIsEmailExist, userController.updateUserById);
 
 module.exports = router;
